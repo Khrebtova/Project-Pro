beb4dbbebd82fa5ad671b8b44b0f0383072817b7
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-06-30 14:21:54 -0700

    added navigation to the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,18 @@
 
+
 import React  from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const Home = ({user, clients, employees, projects}) => {
-  
+  const navigate = useNavigate()
   const loggedInPage = () => {
       return (
         <div>
           <h2>Welcome {user.first_name} {user.last_name}! You have:</h2>
-          <h4>{employees.length} employees</h4>
-          <h4>{clients.length} clients</h4>
-          <h4>{projects.length} projects</h4>          
+          <h4 onClick={()=>navigate('/employees')}>{employees.length} employees</h4>
+          <h4 onClick={()=>navigate('/clients')}>{clients.length} clients</h4>
+          <h4 onClick={()=>navigate('/projects')}>{projects.length} projects</h4>
+          <h4 onClick={()=>navigate('/user')}>{user.todos.length} items in your todo list</h4>          
         </div>
       )
     }
