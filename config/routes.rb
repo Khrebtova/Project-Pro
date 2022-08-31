6bc2d44cbdb60e611a51a798c3112128cf3b3f56
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-08-31 09:50:20 -0700

    fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   namespace :api do
     resources :todos
     resources :projects
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
   end 
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
